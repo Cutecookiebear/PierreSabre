2690a393513bdac35bb532a552744f08559901e0
Cutecookiebear <lucynda.carlier@gmail.com>
2022-11-06 16:11:06 +0100

    creation de la classe Commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,18 @@
+package personnages;
+
+public class Commercant extends Humain {
+
+	public Commercant(String nom, String boisson, int argent) {
+		super(nom, boisson, argent);
+	}
+    public int seFaireExtorquer() {
+    	int argentPerdu=argent;
+    	argent-=argentPerdu;
+    	parler("J'ai tout perdu! Le monde est trop injuste...");
+    	return argentPerdu; 
+    }
+	public void recevoir(int don) {
+		argent+=don;
+		parler(don+" sous! Je te remercie généreux donateur!");
+	}
+}
