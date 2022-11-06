05a2353aba40071fe9dd0b707b8f7d5ac3663d58
Cutecookiebear <lucynda.carlier@gmail.com>
2022-11-06 17:31:19 +0100

    combat entre Ronin et Yakuza

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -12,5 +12,21 @@ public class Ronin extends Humain {
 		beneficiaire.recevoir(argentDonne);
 				
 	}
+	public void provoquer(Yakuza adversaire) {
+		int force = honneur*2;
+		parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand!");
+		if(force>=adversaire.getReputation()) {
+			parler("Je t'ai eu petit Yakuza!");
+			int argentGagne = adversaire.perdre();
+			gagnerArgent(argentGagne);
+		}
+		else {
+			int argentPerdu = argent;
+			perdreArgent(argentPerdu);
+			honneur -= 1;
+			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse ont en pris un coup");
+			adversaire.gagner(argentPerdu);
+		}
+	}
 
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -2,7 +2,11 @@ package personnages;
 
 public class Yakuza extends Humain {
 	private String clan;
-	private int reputation=0;
+	private int reputation=4;
+	
+	public int getReputation() {
+		return reputation;
+	}
 
 	public Yakuza(String nom, String boisson, int argent,String clan) {
 		super(nom, boisson, argent);
@@ -14,6 +18,18 @@ public class Yakuza extends Humain {
 		int argentVole = victime.seFaireExtorquer();
 		gagnerArgent(argentVole);
 		parler("J’ai piqué les "+argentVole+" sous de "+victime.nom+", ce qui me fait "+argent+" sous dans ma poche. Hi ! Hi !");	
+	}
+	public void gagner(int gain) {
+		gagnerArgent(gain);
+		reputation += 1;
+		parler("Ce ronin pensait vraiment battre "+nom+" du clan "+clan+"? Je l'ai dépouillé de ses "+gain+" sous.");
+	}
+	public int perdre() {
+		int argentPerdu = argent;
+		perdreArgent(argentPerdu);
+		reputation -= 1;
+		parler("J'ai perdu mon duel et mes "+argentPerdu+" sous, snif... J'ai déshonoré le clan de "+clan+".");
+		return argentPerdu;
 	}
 
 }
