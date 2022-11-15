98ca04f2e896c1d8655bedc99f57d89f8072cb6a
carlier lucynda <crl4440a@ens.fsi>
2022-11-15 17:49:42 +0100

    traitrepart1

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,10 @@
+package personnages;
+
+public class Traitre extends Samourai {
+
+	public Traitre(String seigneur, String nom, String boisson, int argent) {
+		super(seigneur, nom, boisson, argent);
+		// TODO Auto-generated constructor stub
+	}
+
+}
